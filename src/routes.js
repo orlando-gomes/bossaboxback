86787ae6972791acb897e13259fa5e8dfaafe2c3
Orlando Gomes <orlandosjm@yahoo.com.br>
2020-04-19 14:17:59 -0300

    Testes de deploy

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 import swaggerUi from 'swagger-ui-express';
 import * as swaggerDocument from '../swagger.json';
 
-// import authMiddleware from './app/middlewares/auth';
+import authMiddleware from './app/middlewares/auth';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
@@ -21,7 +21,7 @@ routes.get('/', (req, res) => {
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
-// routes.use(authMiddleware);
+routes.use(authMiddleware);
 
 routes.get('/users', UserController.index);
 routes.get('/users/:id', UserController.show);
