022036dfca0e5200699152812f51a8459f15283c
Orlando Gomes <orlandosjm@yahoo.com.br>
2020-04-22 20:25:30 -0300

    Ajustes para deploy

--- src/routes.js
+++ src/routes.js
@@ -15,7 +15,7 @@ const routes = new Router();
 routes.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 routes.get('/', (req, res) => {
-  return res.json({ msg: 'Bossaboxback hosted operating 100%!' });
+  return res.json({ msg: 'Bossaboxback operating 100%!' });
 });
 
 routes.post('/users', UserController.store);
