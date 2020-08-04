64b57dd11f021362c1305d28c40ce20c2e496318
Orlando Gomes <orlandosjm@yahoo.com.br>
2020-08-04 20:29:08 -0300

    Adicionada https no express

--- src/app.js
+++ src/app.js
@@ -1,6 +1,93 @@
 import './bootstrap';
 
 import express from 'express';
+
+import cors from 'cors';
+
+import routes from './routes';
+
+import './database/databaseLoader';
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+
+    // Para conformidade com a politica de CORS dos navegadores
+    if (process.env.NODE_ENV === 'production') {
+      this.server.use(
+        cors({ origin: 'https://modest-hodgkin-38008e.netlify.app' })
+      );
+    } else {
+      this.server.use(cors());
+    }
+
+    // Em ambiente de produção, poderíamos setar
+    // this.server.use(cors({ origin: 'https://meudominio.com.br' }));
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
+
+/* sem https
+import './bootstrap';
+
+import express from 'express';
+
+import cors from 'cors';
+
+import routes from './routes';
+
+import './database/databaseLoader';
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+
+    // Para conformidade com a politica de CORS dos navegadores
+    if (process.env.NODE_ENV === 'production') {
+      this.server.use(
+        cors({ origin: 'https://modest-hodgkin-38008e.netlify.app' })
+      );
+    } else {
+      this.server.use(cors());
+    }
+
+    // Em ambiente de produção, poderíamos setar
+    // this.server.use(cors({ origin: 'https://meudominio.com.br' }));
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
+*/
+
+/* HTTPS
+import './bootstrap';
+
+import express from 'express';
 import https from 'https';
 import fs from 'fs';
 
@@ -59,48 +146,4 @@ class App {
 }
 
 export default new App().server;
-
-/*
-import './bootstrap';
-
-import express from 'express';
-
-import cors from 'cors';
-
-import routes from './routes';
-
-import './database/databaseLoader';
-
-class App {
-  constructor() {
-    this.server = express();
-
-    this.middlewares();
-
-    this.routes();
-  }
-
-  middlewares() {
-    this.server.use(express.json());
-
-    // Para conformidade com a politica de CORS dos navegadores
-    if (process.env.NODE_ENV === 'production') {
-      this.server.use(
-        cors({ origin: 'https://modest-hodgkin-38008e.netlify.app' })
-      );
-    } else {
-      this.server.use(cors());
-    }
-
-    // Em ambiente de produção, poderíamos setar
-    // this.server.use(cors({ origin: 'https://meudominio.com.br' }));
-  }
-
-  routes() {
-    this.server.use(routes);
-  }
-}
-
-export default new App().server;
-
 */
