c33dd9b0ff54bf7c4feb1ed5a55623db1bb78a39
Orlando Gomes <orlandosjm@yahoo.com.br>
2020-08-04 20:03:27 -0300

    Adicionada https no express

--- src/app.js
+++ src/app.js
@@ -1,6 +1,69 @@
 import './bootstrap';
 
 import express from 'express';
+import https from 'https';
+import fs from 'fs';
+
+import cors from 'cors';
+
+import routes from './routes';
+
+import './database/databaseLoader';
+
+class App {
+  constructor() {
+    this.app = express();
+
+    this.middlewares();
+
+    this.routes();
+
+    if (process.env.NODE_ENV === 'production') {
+      const privateKey = fs.readFileSync(
+        '/etc/letsencrypt/live/orlstechbacks.com/privkey.pem',
+        'utf8'
+      );
+      const certificate = fs.readFileSync(
+        '/etc/letsencrypt/live/orlstechbacks.com/fullchain.pem',
+        'utf8'
+      );
+      const credentials = {
+        key: privateKey,
+        cert: certificate,
+      };
+      this.server = https.createServer(credentials, this.app).listen(443);
+    } else {
+      this.server = this.app;
+    }
+  }
+
+  middlewares() {
+    this.app.use(express.json());
+
+    // Para conformidade com a politica de CORS dos navegadores
+    if (process.env.NODE_ENV === 'production') {
+      this.app.use(
+        cors({ origin: 'https://modest-hodgkin-38008e.netlify.app' })
+      );
+    } else {
+      this.app.use(cors());
+    }
+
+    // Em ambiente de produção, poderíamos setar
+    // this.server.use(cors({ origin: 'https://meudominio.com.br' }));
+  }
+
+  routes() {
+    this.app.use(routes);
+  }
+}
+
+export default new App().server;
+
+/*
+import './bootstrap';
+
+import express from 'express';
 
 import cors from 'cors';
 
@@ -39,3 +102,5 @@ class App {
 }
 
 export default new App().server;
+
+*/
